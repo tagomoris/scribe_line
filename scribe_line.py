da932dc6b7d9ed985192b23a2b4e603d97dada95
TAGOMORI Satoshi <tagomoris@livedoor.jp>
2010-12-15 11:05:13 +0900

    testing keep-alive thrift.transport connection

--- scribe_line.py
+++ scribe_line.py
@@ -44,6 +44,62 @@ host = sys.argv[-3]
 port = sys.argv[-2]
 category = sys.argv[-1]
 
+while True:
+    try:
+        sock = TSocket.TSocket(host=host, port=int(port))
+        transport = TTransport.TFramedTransport(sock)
+        protocol = TBinaryProtocol.TBinaryProtocol(trans=transport, strictRead=False, strictWrite=False)
+        client = scribe.Client(iprot=protocol, oprot=protocol)
+        transport.open()
+
+        lines = []
+
+        while True:
+            if len(lines) < 1:
+                try:
+                    while len(lines) < DEFAULT_SIZE_LOGS_BUFFERED:
+                        lines.append(stdin_obj.readline())
+                except IOError:
+                    if len(lines) == 0 or (len(lines) == 1 and lines[0] == ''):
+                        lines = []
+                        time.sleep(DEFAULT_RETRY_LOG_WATCH)
+                        continue
+            log_entries = [scribe.LogEntry(category=category, message=line) for line in lines]
+
+            while True:
+                result = client.Log(messages=log_entries)
+                if result == scribe.ResultCode.OK:
+                    lines = []
+                    break
+                elif result == scribe.ResultCode.TRY_LATER:
+                    time.sleep(DEFAULT_RETRY_CONNECT)
+                else:
+                    inbuffer_logs.insert(0, line)
+                    raise TTransportException, TTransportException.UNKNOWN, "Unknown result code: %d." % result
+
+        transport.close()
+    except TTransportException, ttex:
+        if ttex.type == TTransportException.UNKNOWN:
+            print "transferring, UNKNOWN error... retry after sleep"
+            time.sleep(DEFAULT_RETRY_WAIT)
+        elif ttex.type == TTransportException.NOT_OPEN:
+            print "transferring, NOT_OPEN error... retry after sleep"
+            time.sleep(DEFAULT_RETRY_WAIT)
+        elif ttex.type == TTransportException.ALREADY_OPEN:
+            print "transferring, ALREADY_OPEN error... retry after sleep"
+            time.sleep(DEFAULT_RETRY_WAIT)
+        elif ttex.type == TTransportException.TIMED_OUT:
+            print "transferring, TIMED_OUT error... retry after sleep"
+            time.sleep(DEFAULT_RETRY_WAIT)
+        elif ttex.type == TTransportException.END_OF_FILE:
+            print "transferring, EOF error... retry after sleep"
+            time.sleep(DEFAULT_RETRY_WAIT)
+        else:
+            raise
+
+sys.exit(1)
+
+
 lines = []
 while True:
     if len(lines) < 1:
