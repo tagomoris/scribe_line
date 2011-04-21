08c10bb3b9668088757089c5b11214b6efc99df2
TAGOMORI Satoshi <tagomoris@livedoor.jp>
2011-04-21 19:47:45 +0900

    fix arguments/options for execution from other scripts. compatibility is lost.

--- scribe_line.sh
+++ scribe_line.sh
@@ -15,42 +15,93 @@
 ##  See the License for the specific language governing permissions and
 ##  limitations under the License.
 
-PYTHON="python"
+# USAGE:
+# scribe_line.sh [options] category /path/to/logfile primary_server[:port] [secondary_server[:port]]
+# OPTIONS:
+#    -p pythonpath
+
+PYTHON="/usr/bin/python"
+
 SCRIBE_LINE_CMD=$(dirname $0)"/scribe_line.py"
-MD5SUM="/usr/bin/md5sum"
-if [ ! -x $MD5SUM ]; then
-    MD5SUM="/sbin/md5"
+DEFAULT_PORT=1463
+
+function usage {
+    cat >&2 <<EOF
+USAGE:
+scribe_line.sh [options] category /path/to/logfile primary_server[:port] [secondary_server[:port]]
+OPTIONS:
+   -p pythonpath
+EOF
+}
+
+while getopts "hp:" flag; do
+    case $flag in
+        \?) OPT_ERROR=1;;
+        h) usage; exit 0;;
+        p) PYTHON="$OPTARG";;
+    esac
+done
+
+shift $(( $OPTIND - 1 ))
+
+if [ ! -x "$PYTHON" ]; then
+    cat >&2 <<EOF
+ERROR: specified python path is not executable: $PYTHON
+EOF
+    OPT_ERROR=1
 fi
 
-DEFAULT_CATEGORY="unknown"
-
-PRIMARY_SERVER="deliver.dh.scribe.admin"
-PRIMARY_PORT="1463"
-
-SECONDARY_SERVER="central.dh.scribe.admin"
-SECONDARY_PORT="1463"
-
-logs_dir=$1
-if [ ! -e $logs_dir ]; then
-    exit 1;
-fi
-basename=$2
-if [ x"$basename" = "x" ]; then
-    exit 1;
-fi
-
-category=$3
+category=$1
 if [ x"$category" = "x" ]; then
-    category=$DEFAULT_CATEGORY
+    cat >&2 <<EOF
+ERROR: category not specified
+EOF
+    ARG_ERROR=1
 fi
 
-opt=$4
-if [ x"$opt" = "x-py24" ]; then
-    PYTHON="/usr/local/bin/python2.4"
+tail_file_path=$2
+if [ ! -f "$tail_file_path" ]; then
+    cat >&2 <<EOF
+ERROR: target log file path not specified or not exists: $tail_file_path
+EOF
+    ARG_ERROR=1
 fi
 
-target_checksum=`echo -n "$logs_dir/$basename" | $MD5SUM`
-tail_file_path=`find $logs_dir -name $basename'*' -type f | xargs ls -t | head -1`
+primary=$3
+if [ x"$primary" = "x" ]; then
+    cat >&2 <<EOF
+ERROR: primary scribed server not specified
+EOF
+    ARG_ERROR=1
+else
+    PRIMARY_SERVER=`echo $primary | cut -d : -s -f 1`
+    PRIMARY_PORT=`echo $primary | cut -d : -s -f 2`
+    if [ x"$PRIMARY_SERVER" = "x" ]; then
+        PRIMARY_SERVER=$primary
+        PRIMARY_PORT=$DEFAULT_PORT
+    fi
+fi
 
-tail -F $tail_file_path | $PYTHON $SCRIBE_LINE_CMD $category $PRIMARY_SERVER $PRIMARY_PORT $SECONDARY_SERVER $SECONDARY_PORT
+secondary=$4
+if [ x"$secondary" = "x" ]; then
+    SECONDARY_UNSPECIFIED=1
+else
+    SECONDARY_SERVER=`echo $secondary | cut -d : -s -f 1`
+    SECONDARY_PORT=`echo $secondary | cut -d : -s -f 2`
+    if [ x"$SECONDARY_SERVER" = "x" ]; then
+        SECONDARY_SERVER=$secondary
+        SECONDARY_PORT=$DEFAULT_PORT
+    fi
+fi
+
+if [ x"$OPT_ERROR" != "x" -o x"$ARG_ERROR" != "x" ]; then
+    usage
+    exit 1
+fi
+
+if [ $SECONDARY_UNSPECIFIED ]; then
+    tail -F $tail_file_path | $PYTHON $SCRIBE_LINE_CMD $category $PRIMARY_SERVER $PRIMARY_PORT
+else
+    tail -F $tail_file_path | $PYTHON $SCRIBE_LINE_CMD $category $PRIMARY_SERVER $PRIMARY_PORT $SECONDARY_SERVER $SECONDARY_PORT
+fi
 exit $?
