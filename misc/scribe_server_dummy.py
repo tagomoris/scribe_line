e7712737eb7a7c769c1b00e4df55fa63edefd994
TAGOMORI Satoshi <tagomoris@gmail.com>
2011-11-15 14:32:23 +0900

    add option for long time bench

--- misc/scribe_server_dummy.py
+++ misc/scribe_server_dummy.py
@@ -13,10 +13,15 @@ from thrift.server import TServer
 from scribe import scribe
 from scribe import ttypes
 
+verbose = True
+port = 1463
 if len(sys.argv) == 2:
-  port = int(sys.argv[1])
+    port = int(sys.argv[1])
+elif len(sys.argv) == 3 and sys.argv[1] == '-q':
+    verbose = False
+    port = int(sys.argv[2])
 else:
-  sys.exit('usage (message is stdin): scribe_client_dummy.py port')
+    sys.exit('usage (message is stdin): scribe_client_dummy.py [-q] port')
 
 from pprint import pprint
 
@@ -26,16 +31,20 @@ class ScribeDummyHandler:
 
     def Log(self, messages):
         msgs = {}
-        for m in messages:
-            if m.category not in msgs:
-                msgs[m.category] = 0
-            msgs[m.category] += 1
-        for k in msgs.keys():
-            sys.stdout.write(k + ':' + str(msgs[k]) + "\n")
-        sys.stdout.flush()
+        if verbose:
+            for m in messages:
+                if m.category not in msgs:
+                    msgs[m.category] = 0
+                msgs[m.category] += 1
+            for k in msgs.keys():
+                sys.stdout.write(k + ':' + str(msgs[k]) + "\n")
+            sys.stdout.flush()
+        else:
+            self.counter += len(messages)
         return ttypes.ResultCode.OK
 
 handler = ScribeDummyHandler()
+
 processor = scribe.Processor(handler)
 transport = TSocket.TServerSocket(port)
 tfactory = TTransport.TFramedTransportFactory()
@@ -43,4 +52,23 @@ pfactory = TBinaryProtocol.TBinaryProtocolFactory(strictRead=False, strictWrite=
 
 server = TServer.TSimpleServer(processor, transport, tfactory, pfactory)
 
-server.serve()
+
+import signal
+
+class SIGHUPException(Exception):
+    pass
+
+def signal_handler(signum, frame):
+    raise SIGHUPException
+
+signal.signal(signal.SIGHUP, signal_handler)
+
+try:
+    server.serve()
+except:
+    pass
+
+sys.stdout.write("\n")
+sys.stdout.write("RECV:" + str(handler.counter) + "\n")
+sys.stdout.flush()
+
